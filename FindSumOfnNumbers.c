9519c16287a7fe6db392824002b4db4197b4fd6c
Yogesh Jangid <49384150+yogesh584@users.noreply.github.com>
2021-10-02 21:38:28 +0530

    Update FindSumOfnNumbers.c

--- FindSumOfnNumbers.c
+++ FindSumOfnNumbers.c
@@ -7,9 +7,14 @@ int main(){
   printf("enter any number : ");
   scanf("%d",&n);
 
-  for (int i = 0;i < n;i++){
-    sum += i;
-  }
+  sum = (n/2)*(n+1)
+  /*
+  previous programe take order of n seconds 
+  by using math formula sum = (n/2)*(2a + (n-1)*d) 
+  where a is starting value of AP (Arithmetic progression) and d is Diffrence between every element 
+  A.P (Arithmetic progression) -> 1,2,3,4,5,6, ...
+  */
+
 
   printf("the sum is : %d",sum);
   return 0;
